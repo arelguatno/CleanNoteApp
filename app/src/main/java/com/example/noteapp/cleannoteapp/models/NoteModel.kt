9ffbd238e8de0f1a4b3026db43ec5b18205b6b9c
Arel Guatno <arelguatno@yahoo.com>
2022-10-08 17:09:49 +0100

    Adapter

---
+++ app/src/main/java/com/example/noteapp/cleannoteapp/models/NoteModel.kt
@@ -0,0 +1,7 @@
+package com.example.noteapp.cleannoteapp.models
+
+data class NoteModel(
+    var header: String? = "",
+    var date: String? = "",
+    var body: String? = ""
+)
--- app/src/main/java/com/example/noteapp/cleannoteapp/presentation/notelist/NoteListAdapter.kt
+++ app/src/main/java/com/example/noteapp/cleannoteapp/presentation/notelist/NoteListAdapter.kt
@@ -1,4 +1,45 @@
 package com.example.noteapp.cleannoteapp.presentation.notelist
 
-class NoteListAdapter{
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.noteapp.cleannoteapp.databinding.ListRowBinding
+import com.example.noteapp.cleannoteapp.models.NoteModel
+
+class NoteListAdapter : PagingDataAdapter<NoteModel, NoteListAdapter.MyViewHolder>(DIFF_CALLBACK) {
+
+    inner class MyViewHolder(val binding: ListRowBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<NoteModel>() {
+            override fun areItemsTheSame(oldItem: NoteModel, newItem: NoteModel): Boolean {
+                return oldItem.body == newItem.body
+            }
+
+            override fun areContentsTheSame(oldItem: NoteModel, newItem: NoteModel): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.binding.header.text = item!!.header
+        holder.binding.date.text = item.date
+        holder.binding.body.text = item.date
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder(
+            ListRowBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
 }
